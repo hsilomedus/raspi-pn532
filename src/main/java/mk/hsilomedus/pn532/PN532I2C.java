864324c2829387e602195a654dc560b276266062
hsilomedus <pancec@yahoo.com>
2014-12-01 00:46:56 +0100

    removed property comments.

--- src/main/java/mk/hsilomedus/pn532/PN532I2C.java
+++ src/main/java/mk/hsilomedus/pn532/PN532I2C.java
@@ -1,9 +1,4 @@
-/*
- * Copyright (C) 2014 by Netcetera AG. All rights reserved. The copyright to the computer program(s)
- * herein is the property of Netcetera AG, Switzerland. The program(s) may be used and/or copied
- * only with the written permission of Netcetera AG or in accordance with the terms and conditions
- * stipulated in the agreement/contract under which the program(s) have been supplied.
- */
+
 package mk.hsilomedus.pn532;
 
 import java.io.IOException;
